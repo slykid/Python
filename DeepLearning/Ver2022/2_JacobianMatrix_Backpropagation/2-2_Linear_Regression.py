c2bfa37e13e1fb863bdddb3db76c02c68e736b90
slykid <slykid@naver.com>
2025-05-02 12:19:30 +0900

    FEAT: [DeepLearning] 2. Jacobian Matrix & Backpropagation * Exercise 4-2. Linear Regression with N-Features 구현 예제 작성

--- DeepLearning/Ver2022/2_JacobianMatrix_Backpropagation/2-2_Linear_Regression.py
+++ DeepLearning/Ver2022/2_JacobianMatrix_Backpropagation/2-2_Linear_Regression.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
 from matplotlib import colormaps
+from matplotlib import cm as cm
 
 np.random.seed(1)
 matplotlib.use("MacOSX")
@@ -67,3 +68,62 @@ ax[0].tick_params(labelsize=20)
 ax[1].axhline(y=t_w, color='darkred', linestyle=':')
 ax[1].axhline(y=t_b, color='darkblue', linestyle=':')
 ax[1].tick_param(labelsize=20)
+
+
+# with N-Features
+N, n_features = 100, 3
+lr = 0.01
+t_W = np.random.uniform(-3, 3, (n_features, 1))
+t_b = np.random.uniform(-3, 3, (1, ))
+
+W = np.random.uniform(-3, 3, (n_features, 1))
+b = np.random.uniform(-3, 3, (1, 1))
+
+## Generate dataset
+x_data = np.random.randn(N, n_features)
+y_data = x_data @ t_W + t_b
+
+J_track, w_track, b_track = [], [], []
+
+for data_idx, (X, y) in enumerate(zip(x_data, y_data)):
+    w_track.append(W)
+    b_track.append(b)
+
+    # forward-propagation
+    X = X.reshape(1, -1)
+    pred = X @ W + b
+
+    J = (y - pred) ** 2
+    J_track.append(J.squeeze())
+
+    # jacobian
+    dJ_dpred = -2 * (y - pred)
+    dpred_dw = X
+    dpred_db = 1
+
+    # back-propagation
+    dJ_dw = dJ_dpred * dpred_dw
+    dJ_db = dJ_dpred * dpred_db
+
+    # parameter update
+    W = W - lr * dJ_dw.T
+    b = b - lr * dJ_db
+
+w_track = np.hstack(w_track)
+b_track = np.concatenate(b_track).flatten()
+
+# visualize result
+fig, axes = plt.subplots(2, 1, figsize=(20, 10))
+axes[0].plot(J_track)
+axes[0].set_ylabel('MSE', fontsize=30)
+axes[0].tick_params(labelsize=20)
+
+cmap = cm.get_cmap('rainbow', lut=n_features)
+for w_idx, (t_w, W) in enumerate(zip(t_W, w_track)):
+    axes[1].axhline(y=t_w, color=cmap(w_idx), linestyle=':')
+    axes[1].plot(W)
+axes[1].axhline(t_b, color='black', linestyle=':')
+axes[1].plot(b_track, color='black')
+axes[1].tick_params(labelsize=20)
+
+plt.show()
