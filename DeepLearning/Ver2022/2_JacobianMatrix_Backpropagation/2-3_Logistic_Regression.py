ddde16cb0a0a505722b9ecc32eac079441da7cc7
slykid <slykid@naver.com>
2025-05-26 23:11:43 +0900

    FEAT: [DeepLearning] 2. Jacobian Matrix & Backpropagation * Exercise 4-3. Logistic Regression 구현 예제 작성

--- DeepLearning/Ver2022/2_JacobianMatrix_Backpropagation/2-3_Logistic_Regression.py
+++ DeepLearning/Ver2022/2_JacobianMatrix_Backpropagation/2-3_Logistic_Regression.py
@@ -9,8 +9,8 @@ matplotlib.use("MacOSX")
 plt.style.use("seaborn-v0_8")
 
 # Set Parameter
-N = 300
-learning_rate = 0.01
+N = 1000
+learning_rate = 0.03
 t_w = np.random.uniform(-3, 3, (1, ))
 t_b = np.random.uniform(-3, 3, (1, ))
 
@@ -20,19 +20,20 @@ b = np.random.uniform(-3, 3, (1, ))
 # Generate Dataset
 decision_boundary = -t_b/t_w
 
-X = np.random.normal(decision_boundary, 1, size=(N, ))
-y = X * t_w + t_b
-y = (X > decision_boundary).astype(np.int)
+x_data = np.random.normal(decision_boundary, 1, size=(N, ))
+y_data = x_data * t_w + t_b
+y_data = (x_data > decision_boundary).astype(int)
 
 fig, ax = plt.subplots(figsize=(10, 5))
-ax.scatter(X, y)
+ax.scatter(x_data, y_data)
 plt.show()
 
-x_range = np.linspace(X.min(), X.max(), N)
+x_range = np.linspace(x_data.min(), x_data.max(), N)
 cmap = plt.get_cmap("rainbow", lut=N)
 
 J_track, w_track, b_track = list(), list(), list()
-for idx, (_x, _y) in enumerate(zip(X, y)):
+
+for idx, (x, y) in enumerate(zip(x_data, y_data)):
     w_track.append(w)
     b_track.append(b)
 
@@ -43,16 +44,15 @@ for idx, (_x, _y) in enumerate(zip(X, y)):
     # plt.show()
 
     # Forward Propagation
-    z = _x * w + b
+    z = x * w + b
     y_pred = 1 / (1 + np.exp(-z))
     J = -(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred))
-
     J_track.append(J)
 
     # Jacobian
     dJ_dpred = (y_pred - y) / (y_pred * (1 - y_pred))
     dpred_dz = y_pred * (1 - y_pred)
-    dz_dw = _x
+    dz_dw = x
     dz_db = 1
 
     # Back Propagation
@@ -64,7 +64,29 @@ for idx, (_x, _y) in enumerate(zip(X, y)):
     w = w - learning_rate * dJ_dw
     b = b - learning_rate * dJ_db
 
-    break
+# Visualize loss
+fig, axes = plt.subplots(2, 1, figsize=(10, 5))
+axes[0].plot(J_track)
+axes[0].set_ylabel("BCCE", fontsize=15)
+axes[0].tick_params(labelsize=15)
 
+axes[1].axhline(y=t_w, color="darkred", linestyle=":")
+axes[1].plot([element for array in w_track for element in array], color="darkred")
+axes[1].axhline(y=t_b, color="darkblue", linestyle=":")
+axes[1].plot([element for array in b_track for element in array], color="darkblue")
+axes[1].tick_params(labelsize=15)
 
+plt.show()
 
+# Decision Boundary 시각화
+# - Decision Boundary를 지키면서 학습을 한다는 사실 확인 가능
+arr_w_track = np.array(w_track)
+arr_b_track = np.array(b_track)
+
+arr_db_track = -arr_b_track/arr_w_track
+db = -t_b/t_w
+
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.axhline(y=db, color="black", linestyle=":")
+ax.plot(arr_w_track, color="black")
+plt.show()
