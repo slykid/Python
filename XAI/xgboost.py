94e2323fdd5e25be6d16bf943fe7cef4cf7a0c1b
slykid <slykid@naver.com>
2025-07-22 23:40:24 +0900

    FEAT: [XAI Study] 1. XGBoost

--- XAI/xgboost.py
+++ XAI/xgboost.py
@@ -1,13 +1,25 @@
+import numpy as np
+import pandas as pd
+
+import graphviz
 from numpy import loadtxt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-from xgboost import XGBClassifier
+from xgboost import XGBClassifier, plot_tree, plot_importance
 
-data = loadtxt("Dataset/pima-indians-diabetes.csv", delimiter=",")
+import matplotlib
+from matplotlib import pyplot as plt
+from matplotlib import rcParams
 
-x_data = data[:, 0:8]
-y_data = data[:, 8]
+matplotlib.use("MacOSX")
+plt.style.use("seaborn-v0_8")
+
+# data = loadtxt("Dataset/pima-indians-diabetes.csv", delimiter=",")
+data = pd.read_csv("Dataset/diabetes.csv")
+
+x_data = data[data.columns[0:8]]
+y_data = data[data.columns[8]]
 
 x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=7)
 
@@ -24,3 +36,29 @@ value = [[1, 161, 72, 35, 0, 28.1, 0.527, 20]]  # 입력 형식이 2차원의 nd
 
 l = model.predict_proba(value)
 print("No diabetes: {:.2f}%\nYes diabetes: {:.2%}".format(l[0][0], l[0][1]))
+
+# Graphviz로 확인하기
+rcParams['figure.figsize'] = (12, 10)
+plot_tree(model)
+plt.show()
+
+# 모델 재학습
+model2 = XGBClassifier(max_depth=2)
+model2.fit(x_train, y_train)
+
+y_pred2 = model.predict(x_test)
+predictions2 = [round(value) for value in y_pred2]
+
+acc2 = accuracy_score(y_test, predictions2)
+print("Accuracy: %.2f%%" % (acc2 * 100))
+
+rcParams['figure.figsize'] = (12, 8)
+plot_tree(model2, fmap="")
+plt.show()
+
+# 피쳐 중요도 측정
+rcParams['figure.figsize'] = (10, 10)
+plot_importance(model2)
+plt.yticks(fontsize=15)
+plt.show()
+
