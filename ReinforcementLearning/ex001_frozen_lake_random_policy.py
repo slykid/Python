266eb19bf93c5621d6d36bbb679c2ed3624e5550
slykid <slykid@naver.com>
2026-03-11 12:36:13 +0900

    FEAT: [ReinforcementLearning] 1. Overview

---
+++ ReinforcementLearning/ex001_frozen_lake_random_policy.py
@@ -0,0 +1,25 @@
+from tqdm import tqdm
+import gymnasium as gym
+env = gym.make("FrozenLake-v1", render_mode="human")
+
+num_episodes = 100
+num_timesteps = 50
+total_reward = 0
+total_timestep = 0
+
+for i in tqdm(range(num_episodes)):
+    state, info = env.reset()
+
+    for t in range(num_timesteps):
+        random_action = env.action_space.sample()
+        new_state, reward, terminated, truncated, info = env.step(random_action)
+        total_reward += reward
+
+        if terminated or truncated:
+            break
+
+    total_timestep += t
+
+print("Number of successful episodes: %d/%d"%(total_reward, num_episodes))
+print("Average number of timesteps per episode: %.2f"%(total_timestep/num_episodes))
+
